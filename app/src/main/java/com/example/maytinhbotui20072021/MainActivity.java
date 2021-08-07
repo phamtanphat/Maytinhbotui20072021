6ec4f0fa836d2a9719da6b3cd542dcb93fb452b5
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-08-07 19:25:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/maytinhbotui20072021/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.maytinhbotui20072021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    /*
+            int a = 5;
+            String b = "10";
+
+            Kiểm tra giá trị truyền vào
+            ctrl + p :xem tham số truyền vào
+
+            Log.d("BBB", a - b);
+
+        Math
+            Làm tròn 0.5
+
+                int value = Math.round(5.5f);
+                Log.d("BBB", "Làm tròn 0.5 = " + value);
+
+            Làm tròn lên
+
+                double value1 = Math.ceil(1.000001);
+                Log.d("BBB","Làm tròn lên " + value1);
+
+            Làm tròn xuống
+
+                 double value2 = Math.floor(1.999999);
+                 Log.d("BBB","Làm tròn xuống " + value2);
+
+            Random
+
+                double value3 = Math.random() * 5;
+                Log.d("BBB","Số ngẫu nhiên " + value3);
+
+            Trị tuyệt đối
+
+                 int value4 = Math.abs(-5);
+                 Log.d("BBB","Trị tuyệt đối " + value4);
+    */
+
+
+
+    }
+
+}
